2e50816c97879cd0c90aaf3189dd57055203a15f
John D'Alessandro <jdealla@gmail.com>
2018-03-27 14:17:25 -0400

    Handled default cases in app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const colors = require('colors');
 const init = require(path.join(__dirname, 'script_init'));
 const search = require(path.join(__dirname, 'script_codeCommit'));
 
@@ -14,9 +15,14 @@ function bbwp(args){
             search(args[1])
             break;
         default:
+            console.log(colors.bold(colors.cyan('\n\nBrooks Bell WebPack Build Tool')))
+            console.log(colors.bold(colors.cyan('\nPlease enter one of the following commands:')))
+            console.log(colors.bold(colors.magenta('\n\tinit [client] [testName] [numberOfVariants] ') + colors.grey('- initializes a new test build in the current directory.')));
+            console.log(colors.bold(colors.magenta('\n\tsearchrepos [repoName]') + colors.grey(' - searches AWS for a repo and clones it locally if found.\n')));
             break;
     }
 }
+
 
 let mainIndex = process.argv.reduce((acc, arg, i) => {
     if (arg.indexOf('app.js') > -1) {
